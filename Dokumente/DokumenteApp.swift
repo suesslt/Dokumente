1e7e37180b29975b2c69daf4e155363fb913ed9d
Thomas Suessli <thommy.suessli@bluewin.ch>
2026-02-12 13:42:19 +0900

    Initial Commit

---
+++ Dokumente/DokumenteApp.swift
@@ -0,0 +1,32 @@
+//
+//  DokumenteApp.swift
+//  Dokumente
+//
+//  Created by Thomas Süssli on 12.02.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DokumenteApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DokumenteTests/DokumenteTests.swift
@@ -0,0 +1,17 @@
+//
+//  DokumenteTests.swift
+//  DokumenteTests
+//
+//  Created by Thomas Süssli on 12.02.2026.
+//
+
+import Testing
+@testable import Dokumente
+
+struct DokumenteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
